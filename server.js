16e88c972abf2ea9041f126265b3697897631184
Glitch (fcc-timestamp-mhjo) <none>
2018-01-13 07:36:05 +0000

    🐝🙉 Updated with Glitch

--- server.js
+++ server.js
@@ -1,37 +1,36 @@
-// server.js
-// where your node app starts
-
 // init project
 var express = require('express');
+var moment = require('moment');
+
 var app = express();
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
+app.get("/:timestamp", function (req, res) {
+  res.json(getTSResult(req));
 });
 
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
+function getTSResult(req) {
+    var ts = req.params.timestamp;
+    var time;
+    
+    if (!isNaN(ts)) {
+      var unixtime = Number(ts);
+      time = moment.unix(unixtime);
+    } else {
+      var timestamp = String(ts);
+      time = moment(timestamp);
+    }
 
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
+    return {
+      unix: time.unix(), 
+      natural: time.format("MMMM DD, YYYY")
+    };
+}
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
